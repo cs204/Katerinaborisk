aed620f53a3b6a1b96218dc77b900eec88c56afe
test <test>
2023-12-22 15:03:02 +0700

    test

---
+++ extensions.py
@@ -0,0 +1,9 @@
+filename = input("File name: ")
+if filename.endswith(".jpg"):
+ print("image/jpeg")
+elif filename.endswish(".jpeg"):
+ print("image/jpeg")
+elif filename.endswish(".png"):
+ print("image/png")
+elif filename.endswish(".pdf"):
+ print("application/pdf")
